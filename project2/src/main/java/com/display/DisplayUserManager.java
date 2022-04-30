688a39d534746152955ffa664b70707fe90e54ee
HurSeungDon <zzaa9999@naver.com>
2022-05-01 04:24:47 +0900

    Create DisplayUserManager.java

---
+++ project2/src/main/java/com/display/DisplayUserManager.java
@@ -0,0 +1,40 @@
+package com.display;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class DisplayUserManager extends JFrame {
+	private final String[] column = {"직번","이름","직급"};
+	
+	private JScrollPane scrolledTable;
+	private JTable manageuser;
+	
+	public DisplayUserManager(){
+		setTitle("User Manager");
+		setSize(1200,900);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+		
+		JPanel tablePanel = new JPanel();
+		DefaultTableModel tableModel = new DefaultTableModel(column,0);
+		
+		manageuser = new JTable(tableModel);
+		manageuser.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		
+		scrolledTable = new JScrollPane(manageuser);
+		this.add(scrolledTable);
+		
+		
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new DisplayUserManager();
+	}
+}
